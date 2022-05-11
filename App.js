1ba2826a625eab885035d7e1f111b4674508175d
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2022-05-11 17:43:26 +0530

    Failure to Connect

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Platform, PermissionsAndroid, FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { AppRegistry } from "react-native";
 import BleManager from "react-native-ble-manager";
+import BLEAdvertiser from 'react-native-ble-advertiser'
+import { NativeEventEmitter, NativeModules } from 'react-native';
+import { stringToBytes } from "convert-string";
+import BluetoothStateManager from 'react-native-bluetooth-state-manager';
+import BluetoothDevices from "react-native-bluetooth-devices";
 
-// const BleManagerModule = NativeModules.BleManager;
-// const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 const DATA = [
   {
@@ -27,21 +32,12 @@ const Item = ({ item, onPress, backgroundColor, textColor }) => (
   </TouchableOpacity>
 );
 
-const App = () => {
-  BleManager.start({ showAlert: false }).then(() => {
-    // Success code
-    console.log("Module initialized");
-  });
+// const UUID = "6ce9768e-c170-7595-3c45-e3e19fe5ed0a";
+// const macID = "B4:E6:2D:BF:4F:37";
+// const data = stringToBytes(UUID);
 
-  BleManager.enableBluetooth()
-  .then(() => {
-    // Success code
-    console.log("The bluetooth is already enabled or the user confirm");
-  })
-  .catch((error) => {
-    // Failure code
-    console.log("The user refuse to enable bluetooth");
-  });
+const App = () => {
+  
   
   const [selectedId, setSelectedId] = useState(null);
 
@@ -59,6 +55,7 @@ const App = () => {
     );
   };
 
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -70,6 +67,63 @@ const App = () => {
     </SafeAreaView>
   );
 };
+
+// function connectToPeripheral(peripheral) {
+//   try {
+//       BleManager.connect(peripheral.id)  //98:F4:AB:6E:F4:86
+//           .then(() => {
+//               console.log("Connected to bluetooth device");
+//               //ToastAndroid.show("Connected to the Bluetooth device", ToastAndroid.SHORT)
+//               saveBluetoothPeripheral(peripheral);
+//               retrieveServicesOfPeripheral(peripheral.id);
+//           })
+//           .catch((err) => {
+//               console.log("Error in Ble Manager connect. Err: " + err);
+//           })
+//   }
+//   catch (err) {
+
+//   }
+// }
+
+function retrieveServicesOfPeripheral(peripheralId) {
+  BleManager.retrieveServices(peripheralId).then((peripheralData) => {
+      console.log('Retrieved peripheral services', JSON.stringify(peripheralData));
+      savePeripheralServices(peripheralData);
+      BleManager.readRSSI(peripheralId).then((rssi) => {
+          console.log('Retrieved actual RSSI value', JSON.stringify(rssi));
+          BleManager.startNotificationUseBuffer(peripheralId, serviceCharacteristic, notifyCharacteristic, 1).then(() => {
+              console.log('Started notification on ' + peripheralId);
+              typeOfLastMessageSent = PATIENT_ADMIT;
+              writeToBluetoothDevice(getPatientAdmitMessage());
+          }).catch((error) => {
+              console.log('Notification error', error);
+          });
+      });
+
+
+      //BleManager.write(peripheralId, serviceCharacteristic, writeCharacteristic, stringToBytes("hello Narayanan"), 512)
+
+      /*
+       BleManager.read(    //Retrieve services has to be called before Read
+           peripheralId,
+           serviceCharacteristic,  //0x1700 //6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+           readCharacteristic   //0x1A00 //6E400003-B5A3-F393-E0A9-E50E24DCCA9E
+         ).then((readData) => {
+           // Success code
+           console.log("Read output: " + readData);
+           let buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
+           let sensorData = buffer.toString()//.readUInt8(2, true);
+           console.log("read sensor data: " + sensorData);
+           decryptData(sensorData);
+         }).catch((error) => {
+           console.log('read error', error);
+         });
+
+       console.log("trying to connect using notify")
+*/
+  });
+}
 
 const styles = StyleSheet.create({
   container: {
